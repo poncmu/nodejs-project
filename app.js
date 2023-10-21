d6cb629410611513bef4d1189d5e72f919953377
poncmu <59277501+poncmu@users.noreply.github.com>
2023-10-21 20:20:50 +0700

    product json

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const chalk = require("chalk");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
-const exp = require("constants");
+const products = require("./data/products.json");
 const productRouter = express.Router();
 
 const app = express();
@@ -17,27 +17,7 @@ app.set("view engine", ejs);
 
 productRouter.route("/").get((req, res) => {
     res.render("products", {
-        products: [{
-                productTitle: "น้ำยาล้างจาน",
-                productDescription: "สูตร 1 ดีเลศ",
-                productPrice: 25,
-            },
-            {
-                productTitle: "น้ำยาล้างจาน",
-                productDescription: "สูตร 2 ดีเลศ",
-                productPrice: 35,
-            },
-            {
-                productTitle: "น้ำยาล้างจาน",
-                productDescription: "สูตร 3 ดีเลศ",
-                productPrice: 45,
-            },
-            {
-                productTitle: "น้ำยาล้างจาน",
-                productDescription: "สูตร 4 ดีเลศ",
-                productPrice: 55,
-            },
-        ],
+        products,
     });
 });
 
