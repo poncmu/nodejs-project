039405d987de3f6086588004c6cb894b1fbd1380
poncmu <59277501+poncmu@users.noreply.github.com>
2023-10-21 16:53:52 +0700

    cm4

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 5000;
 app.use(morgan("combined"));
 app.use(express.static(path.join(__dirname, "/public/")));
 
+app.set("views", "./src/views");
+app.set("view engine", ejs);
+
 app.get("/", (req, res) => {
     res.render("index", {
         username: "PondPond",
